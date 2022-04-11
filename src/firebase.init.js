53734095eca6b2bc3186a3dbd928266323f91dcb
Mehedi4467 <mehedihassan4467@gmail.comm>
2022-04-12 01:55:38 +0600

    tailwind uninstall and firebase install

---
+++ src/firebase.init.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBI47cNVmwTAZlB7d-Eh1Q7F4nrQ2BWCaw",
+    authDomain: "ema-jhone-b09b1.firebaseapp.com",
+    projectId: "ema-jhone-b09b1",
+    storageBucket: "ema-jhone-b09b1.appspot.com",
+    messagingSenderId: "313285541144",
+    appId: "1:313285541144:web:6a1104c5406a039ebfb26d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
