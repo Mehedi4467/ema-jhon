eb31cf88c0a1c873af707ab7bc50d5c8b8f818e5
Mehedi4467 <mehedihassan4467@gmail.comm>
2022-04-12 17:03:01 +0600

    implement log in and siginUp

--- src/Components/LogIn/LogIn.js
+++ src/Components/LogIn/LogIn.js
@@ -1,25 +1,62 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import gLogo from '../../images/google-logo.png';
 import './LogIn.css';
+import auth from '../../firebase.init';
+import { useSignInWithEmailAndPassword, useSignInWithGoogle } from 'react-firebase-hooks/auth';
 
 const LogIn = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const [signInWithGoogle, user, loading] = useSignInWithGoogle(auth);
+    const [signInWithEmailAndPassword, user2, loading2] = useSignInWithEmailAndPassword(auth);
+    const navigate = useNavigate();
+
+    const handelEmailBlur = (event) => {
+        setEmail(event.target.value);
+
+    }
+    const handelPasswordBlur = (event) => {
+        setPassword(event.target.value);
+    }
+
+    const handelSubmitLogIn = (event) => {
+        event.preventDefault();
+        signInWithEmailAndPassword(email, password);
+        console.log(user)
+
+    }
+
+
+    if (user2) {
+        navigate('/');
+    }
+    const signInGoogle = () => {
+        signInWithGoogle();
+        if (user) {
+            console.log(user);
+        }
+    }
+
+
+
     return (
         <div className='signUp sign-in'>
             <h2>Login</h2>
 
             <div className='main-field'>
-                <form >
+                <form onSubmit={handelSubmitLogIn}>
                     <div className='main-input'>
                         <label htmlFor="emailForm">Email</label>
-                        <input type="email" placeholder='Your Email' />
+                        <input onBlur={handelEmailBlur} type="email" placeholder='Your Email' />
                         <label htmlFor="passwordForm">Password</label>
-                        <input type="password" placeholder='Your Password' />
+                        <input onBlur={handelPasswordBlur} type="password" placeholder='Your Password' />
 
                     </div>
 
                     <div className='submit'>
-                        <input type="submit" value="Log In" />
+                        <input type="submit" value={loading2 ? 'Loading........' : 'Log In'} />
                         <p>New to Ema-john? <Link to='/signUp'>Create New Account</Link></p>
                     </div>
 
@@ -34,9 +71,9 @@ const LogIn = () => {
                 </div>
 
                 <div>
-                    <div className="googleSignIn">
+                    <div onClick={signInGoogle} className="googleSignIn">
                         <img src={gLogo} width='30px' height='30px' alt="" />
-                        <p>Continue with Google</p>
+                        <p>{loading ? 'Loading........' : 'Continue with Google'}</p>
                     </div>
                 </div>
             </div>
--- src/Components/SignUp/SignUp.js
+++ src/Components/SignUp/SignUp.js
@@ -1,26 +1,82 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './SignUp.css';
 import gLogo from '../../images/google-logo.png';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
+
 
 const SignUp = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+    const [massage, setMassage] = useState('');
+    const [createUserWithEmailAndPassword, user,
+        loading,
+        error,] = useCreateUserWithEmailAndPassword(auth);
+
+    const navigate = useNavigate();
+
+
+    const handelEmail = (event) => {
+        setEmail(event.target.value);
+
+    }
+
+    const handelPassword = (event) => {
+        setPassword(event.target.value);
+
+
+    }
+
+
+    const handelConfirmPassword = (event) => {
+        setConfirmPassword(event.target.value);
+    }
+
+    if (user) {
+        navigate('/')
+    }
+    if (error) {
+        setMassage(error?.message);
+    }
+
+
+    const handelSubmit = (event) => {
+        event.preventDefault();
+        if (password !== confirmPassword) {
+            setMassage("Oop!! Don't match Password");
+            return;
+        }
+
+        if (password.length < 6) {
+            setMassage("Please Password Length at last 6");
+            return;
+        }
+
+        createUserWithEmailAndPassword(email, password);
+
+
+    }
+
     return (
         <div className='signUp'>
             <h2>Sign Up</h2>
+            <p className='massage'>{massage}</p>
 
             <div className='main-field'>
-                <form >
+                <form onSubmit={handelSubmit}>
                     <div className='main-input'>
                         <label htmlFor="emailForm">Email</label>
-                        <input type="email" placeholder='Your Email' />
+                        <input onBlur={handelEmail} type="email" placeholder='Your Email' />
                         <label htmlFor="passwordForm">Password</label>
-                        <input type="password" placeholder='Your Password' />
+                        <input onBlur={handelPassword} type="password" placeholder='Your Password' />
                         <label htmlFor="confirmForm">Confirm Password</label>
-                        <input type="password" placeholder='Your Confirm Password' />
+                        <input onBlur={handelConfirmPassword} type="password" placeholder='Your Confirm Password' />
                     </div>
 
                     <div className='submit'>
-                        <input type="submit" value="Sign Up" />
+                        <input type="submit" value={loading ? 'Loading....' : 'Sign Up'} />
                         <p>Already have an account? <Link to='/login'>Log In</Link></p>
                     </div>
 
